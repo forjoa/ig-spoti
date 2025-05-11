a5cbb884b3f114dcfec6e2224bcaeb2b5cf33cb1
forjoa <joaquin_trujillo@icloud.com>
2025-05-11 12:06:16 +0200

    lang

---
+++ www/app/Language/en/App.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'nav' => [
+        'home' => 'Home',
+        'my_playlists' => 'My Playlists',
+        'logout' => 'Logout'
+    ],
+    'search' => [
+        'placeholder' => 'Search tracks, albums...',
+        'button' => 'Search'
+    ]
+];
---
+++ www/app/Language/es/App.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'nav' => [
+        'home' => 'Inicio',
+        'my_playlists' => 'Mis Playlists',
+        'logout' => 'Cerrar Sesión'
+    ],
+    'search' => [
+        'placeholder' => 'Buscar canciones, álbumes...',
+        'button' => 'Buscar'
+    ]
+];

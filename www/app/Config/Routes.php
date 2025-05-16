fd9c4251cb606285794d90c71b22e9fd709a7de2
forjoa <joaquin_trujillo@icloud.com>
2025-05-16 23:14:48 +0200

    test unauth routes

--- www/app/Config/Routes.php
+++ www/app/Config/Routes.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Controllers\Auth;
+use App\Controllers\Home;
+use App\Controllers\Profile;
+use App\Controllers\Artist;
+use App\Controllers\Album;
+use App\Controllers\Playlist;
+use App\Controllers\MyPlaylists;
 use CodeIgniter\Router\RouteCollection;
 
 /**
@@ -7,45 +14,45 @@ use CodeIgniter\Router\RouteCollection;
  */
 
 // Landing Page (usa el método index de Home para usuarios no logueados)
-$routes->get('/', 'Home::index');
+$routes->get('/', [Home::class, 'index']);
 
 // Autenticación
-$routes->get('/sign-in', 'Auth::signIn');
-$routes->post('/sign-in', 'Auth::handleSignIn');
-$routes->get('/sign-up', 'Auth::signUp');
-$routes->post('/sign-up', 'Auth::handleSignUp');
-
+$routes->get('/sign-in', [Auth::class, 'signIn']);
+$routes->post('/sign-in', [Auth::class, 'handleSignIn']);
+$routes->get('/sign-up', [Auth::class, 'signUp']);
+$routes->post('/sign-up', [Auth::class, 'handleSignUp']);
 
 // Rutas protegidas (requieren autenticación)
-$routes->group('', ['filter' => 'auth'], static function ($routes) {
-    // Home y Búsqueda
-    $routes->get('/home', 'Home::home'); // Método home() en el controlador Home
-    $routes->get('/search', 'Home::search'); // Método search() para resultados de búsqueda
+// $routes->group('', ['filter' => 'auth'], static function ($routes) {
+// Home y Búsqueda
+$routes->get('/home', [Home::class, 'home']);
+$routes->get('/search', [Home::class, 'search']);
 
-    // Perfil de Usuario
-    $routes->get('/profile', 'Profile::index');
-    $routes->post('/profile/update', 'Profile::update');
-    $routes->post('/profile/delete', 'Profile::delete');
+// Perfil de Usuario
+$routes->get('/profile', [Profile::class, 'index']);
+$routes->post('/profile/update', [Profile::class, 'update']);
+$routes->post('/profile/delete', [Profile::class, 'delete']);
 
-    // Artistas y Álbumes
-    $routes->get('/artist/(:num)', 'Artist::show/$1');
-    $routes->get('/album/(:num)', 'Album::show/$1');
+// Artistas y Álbumes
+$routes->get('/artist/(:num)', [Artist::class, 'show']);
+$routes->get('/album/(:num)', [Album::class, 'show']);
 
-    // Playlists Públicas
-    $routes->get('/playlist/(:num)', 'Playlist::show/$1');
+// Playlists Públicas
+$routes->get('/playlist/(:num)', [Playlist::class, 'show']);
 
-    // Mis Playlists
-    $routes->get('/my-playlists', 'MyPlaylists::index');
-    $routes->get('/my-playlists/(:num)', 'MyPlaylists::show/$1');
-    $routes->post('/create-playlist', 'MyPlaylists::create');
-    $routes->put('/my-playlists/(:num)', 'MyPlaylists::update/$1');
-    $routes->delete('/my-playlists/(:num)', 'MyPlaylists::delete/$1');
-    $routes->put('/my-playlists/(:num)/track/(:num)', 'MyPlaylists::addTrack/$1/$2');
-    $routes->delete('/my-playlists/(:num)/track/(:num)', 'MyPlaylists::removeTrack/$1/$2');
-});
+// Mis Playlists
+$routes->get('/my-playlists', [MyPlaylists::class, 'index']);
+$routes->get('/my-playlists/(:num)', [MyPlaylists::class, 'show']);
+$routes->get('/create-playlist', [MyPlaylists::class, 'create']);
+$routes->post('/create-playlist', [MyPlaylists::class, 'create']);
+$routes->put('/my-playlists/(:num)', [MyPlaylists::class, 'update']);
+$routes->delete('/my-playlists/(:num)', [MyPlaylists::class, 'delete']);
+$routes->put('/my-playlists/(:num)/track/(:num)', [MyPlaylists::class, 'addTrack']);
+$routes->delete('/my-playlists/(:num)/track/(:num)', [MyPlaylists::class, 'removeTrack']);
+// });
 
 // Cierre de Sesión (accesible para todos)
-$routes->get('/logout', 'Auth::logout');
+$routes->get('/logout', [Auth::class, 'logout']);
 
 // Manejo de errores 404
 $routes->set404Override(static function () {
